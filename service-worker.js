11b6df5402a8f57876fd7b64b49d73332c021290
pfebrer <42074085+pfebrer@users.noreply.github.com>
2021-02-01 12:01:07 +0000

    Deploying to build from  @ a40cc79d211a4a12bb0469b1a63657983f5d7206 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fdabfa43f86e8102963c8d0155dc20e3.js"
+  "./precache-manifest.4feedbdb488823e7ce8157df1746ded6.js"
 );
 
 self.addEventListener('message', (event) => {
