f7ca20b7dc326867890981e085d8e77caeff2eb4
Pol <pfebrer96@gmail.com>
2021-02-07 20:45:33 +0100

    :package: Added long description for the python package

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
+"""Configuration for the python package
+
+Notice that most options are indicated in setup.cfg.
+Here we only include options that can't be included in setup.cfg
+"""
 from setuptools import setup
+from pathlib import Path
+
+def readme():
+    readme_path = Path("README.md")
+    if not readme_path.exists():
+        return ""
+    
+    with open(readme_path, "r") as f:
+        readme_content = f.read()
+
+    return readme_content.split("Info for developers")[0]
 
 setup(
     package_data={
         "": ["build/*", "build/static/*/*"],
     },
+    long_description=readme(),
+    long_description_content_type="text/markdown",
 )
